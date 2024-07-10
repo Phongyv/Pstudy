ca8a3100722b4d7846235ce7103a388fe4d8c035
Nguyen Tien Phong <321phong@gmail.com>
2024-07-11 00:30:57 +0700

    heleeo

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,6 +3,15 @@ class HomeController {
     getHome(req,res){
         res.render('home');
     }
+    getLearn(req,res){
+        res.render('learn');
+    }
+    getInfo(req,res){
+        res.render('info');
+    }
+    getDonate(req,res){
+        res.render('donate');
+    }
 }
 
 module.exports = new HomeController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ const HomeController = require('../controllers/homeController')
 
 function route(app){
     app.get('/',HomeController.getHome);
+    app.get('/hoctap',HomeController.getLearn);
+    app.get('/gioithieu',HomeController.getInfo);
+    app.get('/donggop',HomeController.getDonate);
 };
 
 
